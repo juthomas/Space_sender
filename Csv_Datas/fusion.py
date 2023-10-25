c54dd68fa720b5849cfda00c4fb2bbd7d3fd59d1
juthomas <juthomas@student.42.fr>
2023-10-25 21:29:09 +0200

    [ADD] Almost complete day fusion

--- Csv_Datas/fusion.py
+++ Csv_Datas/fusion.py
@@ -1,8 +1,20 @@
 import pandas as pd
 
-# Liste de tous vos fichiers CSV
-# fichiers = ['chemin_vers_fichier1.csv', 'chemin_vers_fichier2.csv', ...]
-fichiers = ["OSCAR0_20231010.csv", "currents0_20231010.csv", "PD0_20231010.csv", "PT0_20231010.csv", "temperatures0_20231010.csv"]
+# Liste de tous les fichiers CSV
+# fichiers = ["OSCAR0_20231010.csv", "currents0_20231010.csv", "PD0_20231010.csv", "PT0_20231010.csv", "temperatures0_20231010.csv"]
+# output = "Fusion0_20231010.csv"
+
+# fichiers = ["OSCAR0_20231011.csv", "currents0_20231011.csv", "PD0_20231011.csv", "PT0_20231011.csv", "temperatures0_20231011.csv"]
+# output = "Fusion0_20231011.csv"
+
+# fichiers = ["OSCAR1_20231011.csv", "currents1_20231011.csv", "PD1_20231011.csv", "PT1_20231011.csv", "temperatures1_20231011.csv"]
+# output = "Fusion1_20231011.csv"
+
+fichiers = ["OSCAR3_20231011.csv", "currents3_20231011.csv", "PD3_20231011.csv", "PT3_20231011.csv", "temperatures3_20231011.csv"]
+output = "Fusion3_20231011.csv"
+
+
+
 
 # Lire le premier fichier
 df_final = pd.read_csv(fichiers[0], sep="; ")
@@ -17,13 +29,14 @@ for fichier in fichiers[1:]:
     df_temp = pd.read_csv(fichier, sep="; ")
     df_temp.drop(columns=['Date', 'Line'], inplace=True)
     
-    # Fusionner sur la colonne "Date; Time"
+    # Fusionner sur la colonne "Time"
     df_final = pd.merge(df_final, df_temp, on='Time', how='inner')
     # df_final = pd.merge_asof(df_final, df_temp, left_index=True, right_index=True, direction='nearest')
 
 
 # Trier le DataFrame final selon le timestamp
 df_final = df_final.sort_values(by='Time')
+df_final['Line'] = df_final.index
 
 # Enregistrer le DataFrame fusionné dans un nouveau fichier CSV
-df_final.to_csv('fichier_fusionne.csv', index=False)
+df_final.to_csv(output, index=False)

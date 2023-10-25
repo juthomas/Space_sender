1f1aa1f23440edfc18436bd66f6ba635ae28bec8
juthomas <juthomas@student.42.fr>
2023-10-26 00:25:38 +0200

    [ADD] Global fusion

--- Csv_Datas/toHeader.py
+++ Csv_Datas/toHeader.py
@@ -12,12 +12,14 @@ args = parser.parse_args()
 df = pd.read_csv(args.fichier_csv, sep=";")
 
 starting_index = 1
+
+list_of_steps = ""
 # Itérez sur les lignes du DataFrame
 for index, row in df.iterrows():
     # print("Index:", index)
     # print("Row data:", row)
     # print("Accès à une colonne spécifique (par exemple 'nom_de_la_colonne'):", row['Time'])
-
+    list_of_steps += f"g_all_data_step_{index + starting_index}, "
     print(f"static const t_data g_all_data_step_{index + starting_index}[] =  {{")
     print(f"	(t_data){{.name = \"Photodiode_1\", .offset = offsetof(t_sensors, photodiode_1), .data_type = INTEGER, .int_data = {row['PD0 (V)']}, .int_delta = 0}},")
     print(f"	(t_data){{.name = \"Photodiode_2\", .offset = offsetof(t_sensors, photodiode_2), .data_type = INTEGER, .int_data = {row['PD1 (V)']}, .int_delta = 0}},")
@@ -37,4 +39,20 @@ for index, row in df.iterrows():
     print(f"	(t_data){{.name = \"Temperature_10\", .offset = offsetof(t_sensors, temperature_10), .data_type = INTEGER, .int_data = 0, .int_delta = 0}},")
     print(f"	(t_data){{.name = \"Microphone\", .offset = offsetof(t_sensors, microphone), .data_type = BINARY, .binary = 0}},")
     print(f"	(t_data){{.name = \"Spectro_current\", .offset = offsetof(t_sensors, spectro_current), .data_type = INTEGER, .int_data = {row['I INA SPECTRO (mA)']}, .int_delta = 0}},//TODO : Manage negative values")#TODO : Manage negative values
+    print(f"	(t_data){{.name = \"Organ_current\", .offset = offsetof(t_sensors, organ_current), .data_type = INTEGER, .int_data = {row['I INA OSCAR (mA)']}, .int_delta = 0}},//TODO : Manage negative values")#TODO : Manage negative values
+    print(f"	(t_data){{.name = \"Vin_current\", .offset = offsetof(t_sensors, vin_current), .data_type = INTEGER, .int_data = {row['I INA DC_BUS (mA)']}, .int_delta = 0}},//TODO : Manage negative values")#TODO : Manage negative values
+    print(f"	(t_data){{.name = \"Q7_current\", .offset = offsetof(t_sensors, q7_current), .data_type = INTEGER, .int_data = {row['I INA Q7 (mA)']}, .int_delta = 0}},//TODO : Manage negative values")#TODO : Manage negative values
+    print(f"	(t_data){{.name = \"5v_current\", .offset = offsetof(t_sensors, t5v_current), .data_type = INTEGER, .int_data = {row['I INA 5V (mA)']}, .int_delta = 0}},//TODO : Manage negative values")#TODO : Manage negative values
+    print(f"	(t_data){{.name = \"3.3v_current\", .offset = offsetof(t_sensors, t3_3v_current), .data_type = INTEGER, .int_data = {row['I INA 3V3 (mA)']}, .int_delta = 0}},//TODO : Manage negative values")#TODO : Manage negative values
+    print(f"	(t_data){{.name = \"Motor_current\", .offset = offsetof(t_sensors, motor_current), .data_type = INTEGER, .int_data = {row['I INA MOTORS (mA)']}, .int_delta = 0}},//TODO : Manage negative values")#TODO : Manage negative values
+    print(f"	(t_data){{.name = \"Carousel_state\", .offset = offsetof(t_sensors, carousel_state), .data_type = INTEGER, .int_data = 0, .int_delta = 0}},")
+    print(f"	(t_data){{.name = \"Lid_state\", .offset = offsetof(t_sensors, lid_state), .data_type = INTEGER, .int_data = 0, .int_delta = 0}},")
+    print(f"	(t_data){{.name = \"Organ_1\", .offset = offsetof(t_sensors, organ_1), .data_type = INTEGER, .int_data = {row['POT1']}, .int_delta = 0}},")
+    print(f"	(t_data){{.name = \"Organ_2\", .offset = offsetof(t_sensors, organ_2), .data_type = INTEGER, .int_data = {row['POT2']}, .int_delta = 0}},")
+    print(f"	(t_data){{.name = \"Organ_3\", .offset = offsetof(t_sensors, organ_3), .data_type = INTEGER, .int_data = {row['POT3']}, .int_delta = 0}},")
+    print(f"	(t_data){{.name = \"Organ_4\", .offset = offsetof(t_sensors, organ_4), .data_type = INTEGER, .int_data = {row['POT4']}, .int_delta = 0}},")
+    print(f"	(t_data){{.name = \"Organ_5\", .offset = offsetof(t_sensors, organ_5), .data_type = INTEGER, .int_data = {row['POT5']}, .int_delta = 0}},")
+    print(f"	(t_data){{.name = \"Organ_6\", .offset = offsetof(t_sensors, organ_6), .data_type = INTEGER, .int_data = {row['POT6']}, .int_delta = 0}}}};")
     print(f"")
+
+print(f"{list_of_steps}")
